ee5936f772e357235bada4293031e7f439cac505
niksata <niki.kolev2006@gmail.com>
2026-03-11 14:07:19 +0200

    Functions

--- main.c
+++ main.c
@@ -1,46 +1,13 @@
 #include <stdio.h>
 
+double Sum(double a , double b) {
+    return a + b;
+}
+
 int main()
 {
-    double beg;
-    double end;
-    double delta;
-    double m;
-    double a;
-    double b;
-
-    scanf("%lf", &beg);
-    scanf("%lf", &end);
-    scanf("%lf", &delta);
-    scanf("%lf", &m);
-    scanf("%lf", &a);
-    scanf("%lf", &b);
-
-    while (end <= beg) {
-        printf("End must be greater than beg. Enter new end: ");
-        scanf("%lf", &end);
-    }
-
-    if (m >= beg && m <= end) {
-        printf("M is fine:");
-    }
-    else {
-        printf("M is not fine,enter new m:");
-        scanf("%lf", &m);
-    }
-
-    for (double x = beg; x <= end; x += delta) {
-        if (x <= m) {
-            printf("%.2lf", b+x);
-        }
-        else {
-            if (4 - x==0) {
-                continue;
-            }
-            printf("%.2lf", (a*x)/(4-x));
-        }
-    }
-
+    printf("%.2lf", Sum(10,20));
 
     return 0;
 }
+
