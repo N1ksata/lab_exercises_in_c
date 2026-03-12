ef2f5fd0c63044286ec6b06458e33b50e5fe0bff
niksata <niki.kolev2006@gmail.com>
2026-03-12 19:07:48 +0200

    Functions

--- exercise_2.c
+++ exercise_2.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 //RUSKI SBORNIK ZADACHI FUNKCII
 
-double fahtograd(double fahren) {
-    double cel = (5.0/9.0)*(fahren-32.03);
-    return cel;
+// double fahtograd(double fahren) {
+//     double cel = (5.0/9.0)*(fahren-32.03);
+//     return cel;
+//
+// }
 
+double inchtomm(double x) {
+    double a = x*25.4;
+    return a;
 }
-
 
 int main()
 {
     //zadacha 201
     //napishete zadacha za preobrazuwane na temp ot gradusi -> celzii (C° = 5/9 (F°–32)
-    double fahren;
-    scanf("%lf", &fahren);
-    printf("Fahrenheit ->C°  : %.2lf" , fahtograd(fahren));
+    // double fahren;
+    // scanf("%lf", &fahren);
+    // printf("Fahrenheit ->C°  : %.2lf" , fahtograd(fahren));
+
+    //zadacha 202
+    //inches to mm
+    double inch;
+    scanf("%lf", &inch);
+    printf("%.1lf", inchtomm(inch));
+
+
 
 }
