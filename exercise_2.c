85ec1f29699d5e111548edc0f5a229bbe23cefa8
niksata <niki.kolev2006@gmail.com>
2026-03-16 21:49:32 +0200

    Functions

--- exercise_2.c
+++ exercise_2.c
@@ -17,8 +17,54 @@
 //     return kms;
 // }
 
-double obem_cilinder(double x, double y) {
-    return 3.14*x*x*y;
+// double obem_cilinder(double x, double y) {
+//     return 3.14*x*x*y;
+// }
+
+// int maxnum(int a , int b ) {
+//     if (a > b) {
+//         return a;
+//     }
+//     else {
+//         return b;
+//     }
+// }
+
+// char compare(int a ,int b ) {
+//     char res;
+//     if (a > b) res = '>';
+//     else if (a < b) res = '<';
+//     else res = '=';
+//     return res;
+// }
+
+// float sopr(float r1, float r2 , int t) {
+//     //r1 -resistor 1
+//     //r2 -resistor 2
+//     //t - vid 1 -posledovatelno 2 ->usporedno
+//     float r;
+//     if (t == 1) {
+//         r = r1+r2 ;
+//     }
+//     else if (t == 2) {
+//         r = r1*r2 / (r1+r2);
+//     }
+//     else {
+//         return -1;
+//     }
+//     return r;
+//
+// }
+
+// float percent(double chislo , double procent) {
+//     return  (chislo * procent)/100;
+// }
+
+unsigned long long findFactorial(int n) {
+    if (n <=1) {
+        return 1;
+    }
+    return n * findFactorial(n - 1);
 }
 
 int main()
@@ -45,10 +91,31 @@ int main()
 
 
     //205 obem na cilinder
-    double r,h;
-    scanf("%lf",&r);
-    scanf("%lf",&h);
-    printf("%.2lf",obem_cilinder(r,h));
+    // double r,h;
+    // scanf("%lf",&r);
+    // scanf("%lf",&h);
+    // printf("%.2lf",obem_cilinder(r,h));
+
+    //206 max_num
+    // int a,b;
+    // scanf("%d%d",&a,&b);
+    // printf("The max num is ->%d" , maxnum(a ,b ));
+
+    //207 zadacha > , <  ,  =
+    // int a,b;
+    // printf("Vuvedi 2 celi chisla");
+    // scanf("%d",&a);
+    // scanf("%d",&b);
+    //
+    // printf("%d %c %d",a,compare(a,b),b);
+
+    //208 namirane na suprotivlenie na veriga
+    //209 chislo v procent
+    //210 factoriel
+
+    int number = 5;
+    printf("%d", findFactorial(number));
+    return 0;
 
 
 }
