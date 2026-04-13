5ff7e044983a9ea9a935e5eeef9fd7c998dd8d0b
niksata <niki.kolev2006@gmail.com>
2026-04-13 11:52:11 +0300

    матрица

--- 2razad.c
+++ 2razad.c
@@ -1,11 +1,50 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+
 int main() {
-    int a = 3 ;
-    int *ip;
+    // int n;
+    // int m;
+    // bool isValid = true;
+    //
+    // printf("Въведете брой редове (N) и колони (M): ");
+    // scanf("%d", &n);
+    // scanf("%d", &m);
+    //
+    // int matrix[n][m];
+    //
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j < m; j++) {
+    //         scanf("%d", &matrix[i][j]);
+    //     }
+    // }
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j < m - 1; j++) {
+    //         if (matrix[i][j] > matrix[i][j+1]) {
+    //             isValid = false;
+    //             break;
+    //         }
+    //         if (!isValid) break;
+    //     }
+    //
+    // }
+    //
+    // for (int  j = 0; j < m; j++) {
+    //     for (int i = 0; i < n - 1; i++) {
+    //         if (matrix[i][j] < matrix[i+1][j]) {
+    //             isValid = false;
+    //             break;
+    //         }
+    //         if (!isValid) break;
+    //     }
+    // }
+    // if (isValid) {
+    //     printf("DA\n");
+    // } else {
+    //     printf("NE\n");
+    // }
 
-    ip = &a;
-    printf("%d\n", a);
-    *ip = 100;
-    printf("%d", a);
 
+
+    return 0;
 }
