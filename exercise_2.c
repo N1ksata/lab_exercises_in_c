6490728c3fa230fc27e8c84a4e5b2e6478642cef
niksata <niki.kolev2006@gmail.com>
2026-04-20 10:42:59 +0300

    C working with files

--- exercise_2.c
+++ exercise_2.c
@@ -1,105 +1,38 @@
 #include <stdio.h>
 
-int main() {
-    // FILE *f;
-    // int N, num;
+int main()
+{
+    // FILE *file;
+    // file = fopen("test.txt", "w");
     //
-    // //a
-    // f = fopen("input", "wb");
-    // printf("N: ");
-    // scanf("%d", &N);
-    // fwrite(&N, sizeof(int), 1, f);
-    //
-    // printf("Enter %d nums: ", N);
-    // for (int i = 0; i < N; i++) {
-    //     scanf("%d", &num);
-    //     fwrite(&num, sizeof(int), 1, f);
+    // if (file == NULL) {
+    //     printf("Error opening file");
+    //     return 1;
     // }
-    // fclose(f);
+    // fprintf(file, "NIKOLAY KOLEW1\n");
+    // fclose(file);
     //
-    // //b
-    // f = fopen("input", "rb");
-    // int even = 0, odd = 0;
-    // fread(&N, sizeof(int), 1, f);
-    //
-    // for (int i = 0; i < N; i++) {
-    //     fread(&num, sizeof(int), 1, f);
-    //     if (num % 2 == 0) even++;
-    //     else odd++;
-    // }
-    // fclose(f);
-    // printf("Even: %d, Odd: %d\n", even, odd);
-    //
-    // //c
-    // f = fopen("input", "rb");
-    // fread(&N, sizeof(int), 1, f);
-    // int a[N];
-    // fread(a, sizeof(int), N, f); // Прочита целия масив наведнъж
-    // fclose(f);
-    //
-    // for (int i = 0; i < N - 1; i++) {
-    //     for (int j = 0; j < N - i - 1; j++) {
-    //         if (a[j] > a[j + 1]) {
-    //             int temp = a[j];
-    //             a[j] = a[j + 1];
-    //             a[j + 1] = temp;
-    //         }
-    //     }
+    // char buffer[100];
+    // file = fopen("test.txt", "r");
+    // if (file == NULL) {
+    //     printf("Error opening file1");
+    //     return 1;
     // }
     //
-    // FILE *fo = fopen("output.txt", "w");
-    // for (int i = 0; i < N; i++) {
-    //     fprintf(fo, "%d ", a[i]);
+    // if (fgets(buffer, 100, file) != NULL) {
+    //     printf("%s", buffer);
     // }
-    // fclose(fo);
     //
-    // printf("Sorted array saved to output.txt\n");
+    // fclose(file);
 
+    FILE *txt = fopen("chislo.txt", "r");
+    int number;
+    fscanf(txt, "%d", &number); // chete "10" ot faila
+    fclose(txt);
 
-    //zad2
-    FILE *f;
-    int n;
+    FILE *bin = fopen("result.bin", "wb"); // "wb" za write_binary
+    fwrite(&number, sizeof(int), 1, bin);
+    fclose(bin);
 
-    // a
-    f = fopen("data.bin", "wb");
-    while (scanf("%d", &n) == 1 && n != -1) {
-        fwrite(&n, sizeof(int), 1, f);
-    }
-    fclose(f);
-
-    // b
-    f = fopen("data.bin", "rb");
-    int e = 0, o = 0;
-    while (fread(&n, sizeof(int), 1, f) == 1) {
-        if (n % 2 == 0) e++;
-        else o++;
-    }
-    fclose(f);
-    printf("Even: %d, Odd: %d\n", e, o);
-
-    // c
-    f = fopen("data.bin", "rb");
-    int a[100], count = 0;
-    while (fread(&a[count], sizeof(int), 1, f) == 1) {
-        count++;
-    }
-    fclose(f);
-
-    for (int i = 0; i < count - 1; i++) {
-        for (int j = 0; j < count - i - 1; j++) {
-            if (a[j] > a[j + 1]) {
-                int t = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = t;
-            }
-        }
-    }
-
-    FILE *out = fopen("output.txt", "w");
-    for (int i = 0; i < count; i++) {
-        fprintf(out, "%d ", a[i]);
-    }
-
-    fclose(out);
     return 0;
 }
---
+++ textfiles_into_binary.c
@@ -0,0 +1,35 @@
+//
+// Created by NIkolay Kolev on 20.04.26.
+//
+
+#include <stdio.h>
+
+int main() {
+
+    FILE *t_out = fopen("chislo.txt", "w");
+    fprintf(t_out, "123");
+    fclose(t_out);
+
+
+    FILE *t_in = fopen("chislo.txt", "r");
+    int n;
+    if (t_in == NULL) return 1;
+
+    fscanf(t_in, "%d", &n); // n e 123
+    fclose(t_in);
+
+
+    n = n * 2; // n stawa 246
+
+    //into binary
+    FILE *b_out = fopen("result.bin", "wb");
+    if (b_out == NULL) return 1;
+
+    // n dvoichen format
+    fwrite(&n, sizeof(int), 1, b_out);
+    fclose(b_out);
+
+    printf("Gotovo! Procheteno 123, zapisano 246 v binaren format.\n");
+
+    return 0;
+}
