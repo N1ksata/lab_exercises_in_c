1c7156735b1bce28dcfcef4a1b5d42e027b85de6
niksata <niki.kolev2006@gmail.com>
2026-04-22 07:56:02 +0300

    C working with files

--- textfiles_into_binary.c
+++ textfiles_into_binary.c
@@ -1,35 +1,70 @@
 //
 // Created by NIkolay Kolev on 20.04.26.
 //
-
 #include <stdio.h>
+#include <string.h>
+
+
+struct Inventory
+    {
+        int tables;
+        int chairs;
+        int cups;
+        int dishes;
+        double total_cost;
+    };
 
 int main() {
 
-    FILE *t_out = fopen("chislo.txt", "w");
-    fprintf(t_out, "123");
-    fclose(t_out);
+    //zad bpe 5to 1,2,5,6
+    //zad1
+    int guests;
+    char item[50];
+
+    struct Inventory bought = {0, 0, 0, 0, 0.0};
+
+    if (scanf("%d", &guests) != 1) return 1;
+
+    while (1) {
+        scanf("%s", item);
+        if (strcmp(item, "PARTY!") == 0) break;
+
+        if (strcmp(item, "Table") == 0) {
+            bought.tables++;
+            bought.total_cost += 42.00;
+        } else if (strcmp(item, "Chair") == 0) {
+            bought.chairs++;
+            bought.total_cost += 13.99;
+        } else if (strcmp(item, "Cups") == 0) {
+            bought.cups++;
+            bought.total_cost += 5.98;
+        } else if (strcmp(item, "Dishes") == 0) {
+            bought.dishes++;
+            bought.total_cost += 21.02;
+        }
+    }
+
+    // Изчисляваме колко комплекта/бройки са нужни според броя гости
+    int need_t = (guests + 7) / 8;  // 1 маса за 8 човека
+    int need_c = guests;            // 1 стол за 1 човек
+    int need_s = (guests + 5) / 6;  // 1 комплект (чаши/чинии) за 6 човека
+
+    // Изход
+    printf("\n%.2f\n", bought.total_cost);
+
+    if (need_t > bought.tables)
+        printf("%d Table\n", need_t - bought.tables);
+
+    if (need_c > bought.chairs)
+        printf("%d Chairs\n", need_c - bought.chairs);
+
+    if (need_s > bought.cups)
+        printf("%d Cups\n", need_s - bought.cups);
+
+    if (need_s > bought.dishes)
+        printf("%d Dishes\n", need_s - bought.dishes);
 
 
-    FILE *t_in = fopen("chislo.txt", "r");
-    int n;
-    if (t_in == NULL) return 1;
-
-    fscanf(t_in, "%d", &n); // n e 123
-    fclose(t_in);
-
-
-    n = n * 2; // n stawa 246
-
-    //into binary
-    FILE *b_out = fopen("result.bin", "wb");
-    if (b_out == NULL) return 1;
-
-    // n dvoichen format
-    fwrite(&n, sizeof(int), 1, b_out);
-    fclose(b_out);
-
-    printf("Gotovo! Procheteno 123, zapisano 246 v binaren format.\n");
 
     return 0;
 }
